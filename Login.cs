c955cb94b16c8583e38b159982fe3477aed3d567
David Munyiri <davidmumyiri2019@gmail.com>
2023-11-27 15:49:16 +0300

    "Added Login class"

---
+++ Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_System
+{
+    internal class Login
+    {
+        public void loginUser()
+        {
+            Console.WriteLine("Enter Username");
+            
+            string userName=Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            string passWord = Console.ReadLine();
+
+        }
+    }
+}
